96d57b7ceb9a30af9f57aefb029c1b687c90d6b7
Luke Fairchild <luke@omadi.com>
2015-12-01 09:15:34 -0700

    Cleanup: remove unused var

--- js/mc/on.js
+++ js/mc/on.js
@@ -6,7 +6,6 @@ var bkEventExecutor      = org.bukkit.plugin.EventExecutor;
 var bkRegisteredListener = org.bukkit.plugin.RegisteredListener;
 var bkEventPackage       = 'org.bukkit.event.';
 var nashorn              = ( typeof Java != 'undefined' );
-var eventTasks           = {};
 
 function getHandlerListForEventType ( eventType ) {
 
