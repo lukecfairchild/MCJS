79230c8e14b1e9bbfc0a112487f48ec9e2c0a0a8
Luke Fairchild <lukecfairchild@gmail.com>
2016-05-29 15:16:27 -0600

    test

--- js/main.js
+++ js/main.js
@@ -119,4 +119,5 @@ MySQL.query( 'main', 'UPDATE player SET rank = ? WHERE username = ?', [
 var player2 = MySQL.query( 'main', 'SELECT rank FROM player WHERE username = ?', [
 	'kookster'
 ] )[ 0 ];
+
 console.log( player2.rank );*/
