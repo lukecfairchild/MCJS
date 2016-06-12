c4bea8c7225100dd1c41aae68c2196b06f7bb528
Luke Fairchild <lukecfairchild@gmail.com>
2016-06-12 16:26:23 -0600

    Global restucture, File support

--- Plugins/lib/init.js
+++ Plugins/lib/init.js
@@ -5,12 +5,14 @@
  * functions and variables needed for the environment
  */
 
-var Require = load( PATH + '/lib/Require.js' );
+var Require = load( PATH + '/lib/globals/Require.js' );
+
 
 /**
  * @function
  * @param {string} path - Specify a target javascript file to load.
  */
+
 var require = Require( PATH, [ 'libpath1', 'libpath2' ] );
 
 
@@ -18,7 +20,8 @@ var require = Require( PATH, [ 'libpath1', 'libpath2' ] );
  * Load all globals
  */
 
-global.Class = require( './lib/Class.js' );
+global.Class = require( './lib/globals/Class.js' );
+global.MCJS  = require( './lib/globals/MCJS.js' );
 
 require( './lib/globals/Object.js' );
 require( './lib/globals/String.js' );
@@ -28,10 +31,13 @@ var Timers = require( './lib/globals/Timers.js' );
 global.setInterval   = Timers.setInterval;
 global.setTimeout    = Timers.setTimeout;
 global.clearAllTasks = Timers.clearAllTasks;
+global.console       = require( './lib/globals/console.js' );
 
-global.console = require( './lib/globals/console.js' );
-global.MCJS    = require( './lib/globals/MCJS.js' );
-global.MC      = require( './lib/globals/MC.js' );
+global.File  = require( './lib/globals/File.js' );
+global.MySQL = require( './lib/globals/MySQL.js' );
+
+
+global.MC   = require( './lib/globals/MC.js' );
 
 
 /*
