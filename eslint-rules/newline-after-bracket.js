6e19132ad434316c5c4b90cae7e56641f2092ac1
Luke Fairchild <luke@omadi.com>
2016-09-28 15:15:15 -0600

    Fix: add custom rules

---
+++ eslint-rules/newline-after-bracket.js
@@ -0,0 +1,123 @@
+'use strict';
+
+module.exports = function ( context ) {
+
+	return {
+		'ArrayExpression' : function ( node ) {
+
+			var rawSource = context.getSourceCode();
+
+			var lines = rawSource.getText().split( '\n' );
+			var line  = lines[ node.loc.end.line ];
+
+			if ( typeof line === 'string' ) {
+				line = line.trim();
+			}
+
+			if ( line
+			&&   node.properties
+			&&   node.properties.length
+			&&   node.parent.type !== 'Property'
+			&&   node.parent.type !== 'ArrayExpression'
+			&&   !line.match( '^[\\}\\)\\]]' ) ) {
+
+				if ( node.parent.type === 'CallExpression' ) {
+					var targetNode = node.parent.parent;
+
+					while ( targetNode ) {
+
+						switch ( targetNode.type ) {
+							case 'Property' : {
+								return;
+							}
+
+							case 'Program' :
+							case 'VariableDeclaration' : {
+								context.report( node, 'A newline is required after a ArrayExpression' );
+
+								return;
+							}
+
+							default : {
+								targetNode = targetNode.parent;
+							}
+						}
+					}
+
+				} else {
+					context.report( node, 'A newline is required after a ArrayExpression' );
+				}
+			}
+		},
+		'ObjectExpression' : function ( node ) {
+
+			var rawSource = context.getSourceCode();
+
+			var lines = rawSource.getText().split( '\n' );
+			var line  = lines[ node.loc.end.line ];
+
+			if ( typeof line === 'string' ) {
+				line = line.trim();
+			}
+
+			if ( line
+			&&   node.properties
+			&&   node.properties.length
+			&&   node.parent.type !== 'Property'
+			&&   node.parent.type !== 'ArrayExpression'
+			&&   !line.match( '^[\\}\\)\\]]' ) ) {
+
+				if ( node.parent.type === 'CallExpression' ) {
+					var targetNode = node.parent.parent;
+
+					while ( targetNode ) {
+
+						switch ( targetNode.type ) {
+							case 'Property' : {
+								return;
+							}
+
+							case 'Program' :
+							case 'VariableDeclaration' : {
+								context.report( node, 'A newline is required after a ObjectExpression' );
+
+								return;
+							}
+
+							default : {
+								targetNode = targetNode.parent;
+							}
+						}
+					}
+
+				} else {
+					context.report( node, 'A newline is required after a ObjectExpression' );
+				}
+			}
+		},
+		'BlockStatement' : function ( node ) {
+
+			var rawSource = context.getSourceCode();
+			var source    = context.getSourceCode( node );
+
+			var lines = rawSource.getText().split( '\n' );
+			var line  = lines[ node.loc.end.line ];
+
+			if ( line
+			&&   node.properties
+			&&   node.properties.length
+			&&   !line.trim().match( '^[\\}\\)\\]]' ) ) {
+
+				if ( !( node.parent.type === 'TryStatement'
+				&&      source.getTokenAfter( node ).value === 'catch' )
+				&&   !( node.parent.type === 'CatchClause'
+				&&      source.getTokenAfter( node ).value === 'finally' )
+				&&   !( node.parent.type === 'IfStatement'
+				&&     source.getTokenAfter( node ).value === 'else' ) ) {
+
+					context.report( node, 'A newline is required after a BlockStatement' );
+				}
+			}
+		}
+	};
+};
---
+++ eslint-rules/newline-after-function.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function ( context ) {
+
+	return {
+		'FunctionExpression' : function ( node ) {
+
+			var source = context.getSourceCode( node );
+			var text   = source.getText( node ).split( '\n' );
+
+			if ( text.length > 1
+			&&   text[ 1 ] !== '' ) {
+				context.report( node, 'newline is required after a FunctionExpression' );
+			}
+		}
+	};
+};
